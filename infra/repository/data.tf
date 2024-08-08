9511b1d209c5d662111657a7b46a88f5b058a97a
christian-calabrese <christian.calabrese@pagopa.it>
2024-08-08 15:07:49 +0200

    fix: repository and identity apply fixes

--- infra/repository/data.tf
+++ infra/repository/data.tf
@@ -1,9 +1,9 @@
 data "azurerm_user_assigned_identity" "identity_prod_ci" {
-  name                = "${local.project}-typescript-github-ci-identity"
+  name                = "${local.project}-ipatente-github-ci-identity"
   resource_group_name = local.identity_resource_group_name
 }
 
 data "azurerm_user_assigned_identity" "identity_prod_cd" {
-  name                = "${local.project}-typescript-github-cd-identity"
+  name                = "${local.project}-ipatente-github-cd-identity"
   resource_group_name = local.identity_resource_group_name
 }
