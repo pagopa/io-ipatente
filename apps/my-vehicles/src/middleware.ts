ce382e675a692f951541a2140c9a2b95f57ac0bc
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2024-08-09 14:32:51 +0200

    [IOPAE-1333] First FIMS integration with next-auth library (#2)

---
+++ apps/my-vehicles/src/middleware.ts
@@ -0,0 +1,41 @@
+import { auth } from "@/auth";
+import { NextRequest, NextResponse, userAgent } from "next/server";
+
+const isBrowserContext = (request: NextRequest) =>
+  !!userAgent(request).browser.name;
+
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/api/auth/callback/fims") {
+    if (!isBrowserContext(request)) {
+      const redirectUrl = request.nextUrl;
+      redirectUrl.pathname = "/api/auth/callback/fims/cookies";
+      request.cookies
+        .getAll()
+        .forEach((cookie) =>
+          redirectUrl.searchParams.set(cookie.name, cookie.value),
+        );
+      return NextResponse.redirect(redirectUrl);
+    }
+  } else {
+    const session = await auth();
+    if (!session) {
+      return NextResponse.rewrite(
+        new URL("/api/auth/signin", request.nextUrl.origin),
+      );
+    }
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api/auth (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api/auth|_next/static|_next/image|favicon.ico).*)",
+    "/api/auth/callback/fims",
+  ],
+};
--- apps/my-vehicles/src/pages/_app.tsx
+++ apps/my-vehicles/src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
+import "@/styles/globals.css";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
