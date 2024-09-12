3ad62c999d062df0756fb9450a11254776a6d997
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-09-12 18:19:40 +0200

    update test component

--- apps/my-vehicles/src/components/test-component.tsx
+++ apps/my-vehicles/src/components/test-component.tsx
@@ -1,10 +1,10 @@
+// Used to check vitest correct behaviour on aliases
 import { getConfiguration } from "@/config";
 
 /**
  * Test purpose react component
- * Used to check vitest correct behaviour (especially for aliases)
  **/
 export const TestComponent = () => {
-  const x = getConfiguration().IS_BROWSER;
-  return <div id="test-component">{x}</div>;
+  const isBrowser = getConfiguration().IS_BROWSER;
+  return <div id="test-component">{isBrowser}</div>;
 };
