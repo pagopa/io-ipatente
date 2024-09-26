cf3417f76e3880110860fd7f10181b5ec38d45f4
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2024-09-26 14:41:51 +0200

    Update apps/my-vehicles/types/i18next.d.ts
    Co-authored-by: Alessandro Dell'Oste <alessandro.delloste1@gmail.com>

--- apps/my-vehicles/types/i18next.d.ts
+++ apps/my-vehicles/types/i18next.d.ts
@@ -1,10 +1,3 @@
-/**
- * If you want to enable locale keys typechecking and enhance IDE experience.
- *
- * Requires `resolveJsonModule:true` in your tsconfig.json.
- *
- * @link https://www.i18next.com/overview/typescript
- */
 import "i18next";
 
 // resources.ts file is generated with `npm run toc`
