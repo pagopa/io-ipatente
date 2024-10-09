85b0a4fc17869ef9ee0de34beca8fcb07a2c5911
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-10-09 16:11:05 +0200

    improvements

--- packages/ui/lib/components/operation-result/OperationResult.tsx
+++ packages/ui/lib/components/operation-result/OperationResult.tsx
@@ -1,5 +1,5 @@
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import Button, { ButtonProps } from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { ReactNode } from "react";
@@ -15,16 +15,9 @@ export interface OperationResultProps {
   title: string;
 }
 
-export interface OperationResultAction {
-  /** Element placed after button label */
-  endIcon?: ReactNode;
-  /** Action button label */
+type OperationResultAction = {
   label: string;
-  /** Event triggered on action button click */
-  onClick: () => void;
-  /** Element placed before button label */
-  startIcon?: ReactNode;
-}
+} & Pick<ButtonProps, "endIcon" | "onClick" | "startIcon">;
 
 export const DATA_TEST_ID_OPERATION_RESULT_PREFIX =
   "io-ipatente-operation-result";
@@ -38,41 +31,29 @@ export const OperationResult = ({
 }: OperationResultProps) => (
   <Stack
     alignItems="center"
-    direction="column"
     justifyContent="center"
     padding={3}
+    spacing={4}
     textAlign="center"
   >
-    <Stack
-      alignItems="center"
-      direction="column"
-      justifyContent="center"
-      spacing={4}
-    >
-      {illustration && <Box>{illustration}</Box>}
-      <Stack
-        alignItems="center"
-        direction="column"
-        justifyContent="center"
-        spacing={1}
-      >
-        <Typography fontWeight={700} variant="h5">
-          {title}
-        </Typography>
-        {description && <Typography variant="body2">{description}</Typography>}
-      </Stack>
-      {action && (
-        <Button
-          data-testid={`${DATA_TEST_ID_OPERATION_RESULT_PREFIX}-action`}
-          endIcon={action.endIcon}
-          onClick={action.onClick}
-          size="small"
-          startIcon={action.startIcon}
-          variant="contained"
-        >
-          {action.label}
-        </Button>
-      )}
+    {illustration && <Box>{illustration}</Box>}
+    <Stack alignItems="center" justifyContent="center" spacing={1}>
+      <Typography fontWeight={700} variant="h5">
+        {title}
+      </Typography>
+      {description && <Typography variant="body2">{description}</Typography>}
     </Stack>
+    {action && (
+      <Button
+        data-testid={`${DATA_TEST_ID_OPERATION_RESULT_PREFIX}-action`}
+        endIcon={action.endIcon}
+        onClick={action.onClick}
+        size="small"
+        startIcon={action.startIcon}
+        variant="contained"
+      >
+        {action.label}
+      </Button>
+    )}
   </Stack>
 );
