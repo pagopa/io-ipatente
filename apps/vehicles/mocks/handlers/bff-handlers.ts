fe0ec2b5e33d9d6c7de4e59aef98df8c9fb4a438
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-10-29 12:41:04 +0100

    setup mocks for both bff and external apis

--- apps/vehicles/mocks/handlers/bff-handlers.ts
+++ apps/vehicles/mocks/handlers/bff-handlers.ts
@@ -9,17 +9,22 @@ faker.seed();
 export const buildHandlers = () => {
   const configuration = getConfiguration();
   const baseURL =
-    configuration.BACKEND_API_BASE_URL + configuration.BACKEND_API_BASE_PATH;
+    configuration.BFF_API_BASE_URL + configuration.BFF_API_BASE_PATH;
 
   return [
     http.get(`${baseURL}/infoVeicoli`, () => {
       const resultArray = [
         HttpResponse.json(getInfoVeicoli200Response(), { status: 200 }),
-        HttpResponse.json({ status: 400 }),
-        HttpResponse.json({ status: 401 }),
-        HttpResponse.json({ status: 403 }),
-        HttpResponse.json({ status: 404 }),
-        HttpResponse.json({ status: 500 }),
+        HttpResponse.json({ description: `Bad request` }, { status: 400 }),
+        HttpResponse.json({ description: `Unauthorized` }, { status: 401 }),
+        HttpResponse.json({ description: `Forbidden` }, { status: 403 }),
+        HttpResponse.json({ description: `Not found` }, { status: 404 }),
+        HttpResponse.json(
+          {
+            description: `Internal server error`,
+          },
+          { status: 500 },
+        ),
       ];
       return resultArray[0];
     }),
--- apps/vehicles/mocks/handlers/index.ts
+++ apps/vehicles/mocks/handlers/index.ts
@@ -1,15 +1,19 @@
 import { getConfiguration } from "@/config";
 import { HttpHandler } from "msw";
 
-import { buildHandlers as backendHandlers } from "./backend-handlers";
+import { buildHandlers as bffHandlers } from "./bff-handlers";
+import { buildHandlers as extHandlers } from "./ext-handlers";
 
 /** List of handlers managed by MSW */
 export const getHandlers = () => {
   const config = getConfiguration();
   const handlers: HttpHandler[] = [];
 
-  if (config.BACKEND_API_MOCKING) {
-    handlers.push(...backendHandlers());
+  if (config.BFF_API_MOCKING) {
+    handlers.push(...bffHandlers());
+  }
+  if (config.EXT_API_MOCKING) {
+    handlers.push(...extHandlers());
   }
   return handlers;
 };
