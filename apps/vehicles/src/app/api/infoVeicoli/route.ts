c9bb2110e10b0ee49e40fab4327088985b8a8927
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-10-29 12:45:43 +0100

    wrap up infoVeicoli api, with unit tests

--- apps/vehicles/src/app/api/infoVeicoli/route.ts
+++ apps/vehicles/src/app/api/infoVeicoli/route.ts
@@ -1,18 +1,46 @@
+import { Veicolo } from "@/generated/openapi";
 import { retrieveVehicles } from "@/lib/bff/business";
-import { handleInternalErrorResponse } from "@/lib/bff/errors";
-import { withJWTAuthHandler } from "@/lib/bff/wrappers";
+import {
+  handleBadRequestErrorResponse,
+  handleInternalErrorResponse,
+} from "@/lib/bff/errors";
+import { withJWTAuthAndVoucherHandler } from "@/lib/bff/with-jwt-auth-voucher-handler";
+import { Voucher } from "@io-ipatente/core";
+import { ZodiosError } from "@zodios/core";
+import { AxiosError } from "axios";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 import { CustomUser } from "../../../../types/next-auth";
 
 /**
  * @description Retrieve user vehicles
  */
-export const GET = withJWTAuthHandler(
-  async (request: NextRequest, { user }: { user: CustomUser }) => {
+export const GET = withJWTAuthAndVoucherHandler(
+  async (
+    request: NextRequest,
+    { user, voucher }: { user: CustomUser; voucher: Voucher },
+  ) => {
     try {
-      const res = await retrieveVehicles(user.fiscalCode);
-      return NextResponse.json(res);
+      const res = await retrieveVehicles(voucher.access_token, user.fiscalCode);
+      const vehicles = z.array(Veicolo).safeParse(res);
+
+      if (vehicles.success) {
+        return NextResponse.json(vehicles.data);
+      }
+
+      if (res instanceof AxiosError) {
+        return NextResponse.json(
+          { detail: res.message, status: res.status },
+          { status: res.status },
+        );
+      }
+
+      if (res instanceof ZodiosError) {
+        return handleBadRequestErrorResponse(res.message);
+      }
+
+      return handleInternalErrorResponse("VehiclesRetrieveError", res);
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(
