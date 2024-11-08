d1f7811818e048be2fcf29ab85a6832674c0803d
Gianmaria Scorza <gianmaria@scorza.dev>
2024-11-08 16:28:08 +0100

    OIDC_MOCK_FORCED_ENABLE config, in order to explicitly ebnable OIDC issuer mocks

--- apps/vehicles/src/app/api/auth/mocks/oidc/route.ts
+++ apps/vehicles/src/app/api/auth/mocks/oidc/route.ts
@@ -2,8 +2,12 @@ import { getMockForbiddenResponse } from "@/app/api/auth/mocks/mocks-util";
 import { getConfiguration } from "@io-ipatente/core";
 import { NextResponse } from "next/server";
 
+// GET without params are statics, this will force the route to be dynamic
+export const dynamic = "force-dynamic";
+
 export async function GET() {
-  if (getConfiguration().IS_PRODUCTION) return getMockForbiddenResponse();
+  if (!getConfiguration().OIDC_MOCK_FORCED_ENABLE)
+    return getMockForbiddenResponse();
 
   return NextResponse.json({}, { status: 200 });
 }
