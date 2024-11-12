ac2f5ee1153d4d448d660db0f4a2189768950f54
Gianmaria Scorza <gianmaria@scorza.dev>
2024-11-12 23:15:31 +0100

    app insight only in production

--- apps/vehicles/src/instrumentation.ts
+++ apps/vehicles/src/instrumentation.ts
@@ -3,7 +3,10 @@
  * https://pagopa.atlassian.net/wiki/spaces/DevEx/pages/1086619751/Azure+e+tracing+per+applicativi+NodeJS
  */
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
+  if (
+    process.env.NODE_ENV === "production" &&
+    process.env.NEXT_RUNTIME === "nodejs"
+  ) {
     await import("./instrumentation.node");
     console.info("Instrumentation for Node.js runtime is set!");
   }
