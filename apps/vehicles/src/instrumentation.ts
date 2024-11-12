aadaa756803f35345552ba15f4fa15d7b686e977
Gianmaria Scorza <gianmaria@scorza.dev>
2024-11-12 11:29:49 +0100

    otel cleanUp

--- apps/vehicles/src/instrumentation.ts
+++ apps/vehicles/src/instrumentation.ts
@@ -1,11 +1,25 @@
-import { AzureMonitorTraceExporter } from "@azure/monitor-opentelemetry-exporter";
+import type { SpanExporter } from "@opentelemetry/sdk-trace-base";
+
 import { registerOTel } from "@vercel/otel";
 
 export async function register() {
-  registerOTel({
-    serviceName: "io-p-itn-ipatente-vehicles-app-01",
-    traceExporter: new AzureMonitorTraceExporter({
-      connectionString: process.env["AI_SDK_CONNECTION_STRING"],
-    }),
-  });
+  if (process.env.APP_ENV === "production") {
+    let traceExporter: SpanExporter | undefined;
+
+    if (process.env.NEXT_RUNTIME === "nodejs") {
+      const { AzureMonitorTraceExporter } = await import(
+        "@azure/monitor-opentelemetry-exporter"
+      );
+      traceExporter = new AzureMonitorTraceExporter({
+        connectionString: process.env["AI_SDK_CONNECTION_STRING"],
+        // you can read from ENV if you prefer to
+        // connectionString: process.env.APP_INSIGHTS_CONNECTION_STRING,
+      });
+    }
+
+    registerOTel({
+      serviceName: "io-p-itn-ipatente-vehicles-app-01",
+      traceExporter,
+    });
+  }
 }
