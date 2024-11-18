ccf6e225023e1addfcbd573ab1e79b22bed48351
Stefano.D'aversa <stefano.daversa@pagopa.it>
2024-11-18 11:33:04 +0100

    infra keepalive config

--- infra/resources/_modules/licences_app_service/locals.tf
+++ infra/resources/_modules/licences_app_service/locals.tf
@@ -41,6 +41,14 @@ locals {
       INTEROP_CLIENT_ASSERTION_PURPOSE_ID = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-PURPOSE-ID-LICENCES)"
       INTEROP_CLIENT_ASSERTION_PK         = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-PK-LICENCES)"
 
+      // Fetch keepalive
+      FETCH_KEEPALIVE_ENABLED             = "true"
+      FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL   = "110000"
+      FETCH_KEEPALIVE_MAX_SOCKETS         = "40"
+      FETCH_KEEPALIVE_MAX_FREE_SOCKETS    = "10"
+      FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
+      FETCH_KEEPALIVE_TIMEOUT             = "60000"
+
       # Logs
       AI_SDK_CONNECTION_STRING = var.ai_connection_string
     }
