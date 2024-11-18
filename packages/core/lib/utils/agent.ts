497ec897bdcec892adc84b0fc50130a47fff8af5
Stefano.D'aversa <stefano.daversa@pagopa.it>
2024-11-18 11:32:02 +0100

    keepalive config

---
+++ packages/core/lib/utils/agent.ts
@@ -0,0 +1,30 @@
+import { AxiosRequestConfig } from "axios";
+import * as http from "http";
+import * as https from "https";
+
+import { getConfiguration } from "../config";
+
+export const getKeepAliveAxiosConfig = (): {
+  axiosConfig: AxiosRequestConfig;
+} => {
+  const {
+    FETCH_KEEPALIVE_MAX_FREE_SOCKETS,
+    FETCH_KEEPALIVE_MAX_SOCKETS,
+    FETCH_KEEPALIVE_TIMEOUT,
+  } = getConfiguration();
+  return {
+    axiosConfig: {
+      httpAgent: new http.Agent({
+        keepAlive: true,
+        maxFreeSockets: FETCH_KEEPALIVE_MAX_FREE_SOCKETS,
+        maxSockets: FETCH_KEEPALIVE_MAX_SOCKETS,
+      }),
+      httpsAgent: new https.Agent({
+        keepAlive: true,
+        maxFreeSockets: FETCH_KEEPALIVE_MAX_FREE_SOCKETS,
+        maxSockets: FETCH_KEEPALIVE_MAX_SOCKETS,
+      }),
+      timeout: FETCH_KEEPALIVE_TIMEOUT,
+    },
+  };
+};
--- packages/core/lib/utils/index.ts
+++ packages/core/lib/utils/index.ts
@@ -1,2 +1,3 @@
+export * from "./agent";
 export * from "./constants";
 export * from "./errors";
