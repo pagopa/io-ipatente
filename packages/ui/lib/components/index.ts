e2081c860f0200b960269584d37ffe0c247d8c1d
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-11-22 17:48:41 +0100

    add implementation

--- packages/ui/lib/components/index.ts
+++ packages/ui/lib/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./assistance-info";
 export * from "./card-carousel";
 export * from "./card-info";
+export * from "./consent-view";
 export * from "./dialog";
 export * from "./empty-state";
 export * from "./floating-button";
