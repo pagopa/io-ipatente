e4fde3924f5a0a1ab8f694c8ea31bef81947e39c
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2024-11-25 15:40:40 +0100

    show licence points in the licenses page

--- apps/licences/src/components/licences/ListItemLicence.tsx
+++ apps/licences/src/components/licences/ListItemLicence.tsx
@@ -1,4 +1,6 @@
 import { DatiPatente } from "@/generated/bff-openapi";
+import { LICENCE_TOTAL_POINTS } from "@/utils/constants";
+import { isDueDateValid } from "@io-ipatente/core";
 import { BadgeProps, ListItemAction } from "@io-ipatente/ui";
 import { useTranslation } from "next-i18next";
 import { useMemo } from "react";
@@ -16,27 +18,39 @@ export const ListItemLicence = ({
 }: ListItemLicenceProps) => {
   const { t } = useTranslation();
 
-  const { dataScadenza, numeroPatente } = data;
+  const { dataScadenza, numeroPatente, saldoPunti } = data;
 
   const badges = useMemo<BadgeProps[]>(() => {
-    const isValid =
-      new Date(dataScadenza ?? "").setHours(0, 0, 0, 0) >=
-      new Date().setHours(0, 0, 0, 0);
+    const isValid = dataScadenza ? isDueDateValid(dataScadenza) : false;
+
+    const badgeLicenceStatus: BadgeProps = {
+      color: isValid ? "success" : "warning",
+      icon: isValid ? "tickCircleBold" : "warning2Bold",
+      label: isValid ? t("licences.valid") : t("licences.expired"),
+      size: "small",
+    };
+
+    const badgeLicencePoints: BadgeProps | undefined = saldoPunti
+      ? {
+          color: "info",
+          icon: "driveLicense",
+          label: t("licences.points", {
+            total: LICENCE_TOTAL_POINTS,
+            value: saldoPunti,
+          }),
+          size: "small",
+        }
+      : undefined;
 
     return [
-      {
-        color: isValid ? "success" : "warning",
-        icon: isValid ? "tickCircleBold" : "warning2Bold",
-        label: isValid ? t("licences.valid") : t("licences.expired"),
-        size: "small",
-      },
+      badgeLicenceStatus,
+      ...(badgeLicencePoints ? [badgeLicencePoints] : []),
     ];
-  }, [dataScadenza, t]);
+  }, [dataScadenza, saldoPunti, t]);
 
   return (
     <ListItemAction
       badges={badges}
-      icon="driveLicense"
       label={label}
       onClick={() => onClick(numeroPatente)}
       value={numeroPatente}
