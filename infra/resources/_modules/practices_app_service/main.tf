60c6b08fc04263e2044e0c3ac98d5c55fde0e2d0
Gianmaria Scorza <gianmaria@scorza.dev>
2024-11-26 12:25:00 +0100

    create practices appService

---
+++ infra/resources/_modules/practices_app_service/main.tf
@@ -0,0 +1,68 @@
+#################
+#  App Service  #
+#################
+
+module "practices" {
+  source = "github.com/pagopa/dx//infra/modules/azure_app_service?ref=main"
+
+  environment = {
+    prefix          = var.prefix
+    env_short       = var.env_short
+    location        = var.location
+    domain          = var.domain
+    app_name        = "practices"
+    instance_number = "01"
+  }
+
+  resource_group_name = var.resource_group_name
+  health_check_path   = "/api/info"
+  node_version        = 20
+
+  subnet_cidr                          = var.practices_snet_cidr
+  subnet_pep_id                        = var.peps_snet_id
+  private_dns_zone_resource_group_name = var.private_dns_zone_resource_group_name
+  virtual_network = {
+    name                = var.virtual_network.name
+    resource_group_name = var.virtual_network.resource_group_name
+  }
+
+  app_settings = merge(local.practices.base_app_settings, local.practices.prod_app_setting)
+
+  slot_app_settings = merge(local.practices.base_app_settings, local.practices.staging_app_setting)
+
+  sticky_app_setting_names = local.practices.sticky_settings
+
+  tier = local.practices.tier
+
+  tags = var.tags
+}
+## Apply after create the app service
+# module "practices_app_service_roles" {
+#   source       = "github.com/pagopa/dx//infra/modules/azure_role_assignments?ref=main"
+#   principal_id = module.practices.app_service.app_service.principal_id
+
+#   key_vault = [
+#     {
+#       name                = var.key_vault_name
+#       resource_group_name = var.resource_group_name
+#       roles = {
+#         secrets = "reader"
+#       }
+#     }
+#   ]
+# }
+
+# module "practices_app_service_staging_roles" {
+#   source       = "github.com/pagopa/dx//infra/modules/azure_role_assignments?ref=main"
+#   principal_id = module.practices.app_service.app_service.slot.principal_id
+
+#   key_vault = [
+#     {
+#       name                = var.key_vault_name
+#       resource_group_name = var.resource_group_name
+#       roles = {
+#         secrets = "reader"
+#       }
+#     }
+#   ]
+# }
