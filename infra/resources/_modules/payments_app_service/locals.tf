f7b938d8418f7a9e2e623c5f218f2c8eb5fd518a
Gianmaria Scorza <gianmaria@scorza.dev>
2024-11-26 14:34:18 +0100

    [IOPAE-1602] infra payments practices (#151)

---
+++ infra/resources/_modules/payments_app_service/locals.tf
@@ -0,0 +1,74 @@
+
+locals {
+  payments = {
+    tier = "standard"
+    base_app_settings = {
+      NODE_ENV                 = "production"
+      WEBSITE_RUN_FROM_PACKAGE = "1"
+
+      AUTH_SECRET     = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=AUTH-SECRET-PAYMENTS)"
+      AUTH_TRUST_HOST = true
+      AUTH_URL        = "https://payments.ipatente.io.pagopa.it/"
+
+      OIDC_ISSUER_URL          = "https://payments.ipatente.io.pagopa.it/api/auth/mocks/oidc"
+      OIDC_CLIENT_ID           = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-CLIENT-ID-PAYMENTS)"
+      OIDC_CLIENT_SECRET       = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-CLIENT-SECRET-PAYMENTS)"
+      OIDC_CLIENT_REDIRECT_URI = "https://payments.ipatente.io.pagopa.it/api/auth/callback/fims"
+
+      OIDC_MOCK_JWT_PRIVATE_KEY = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-JWT-PRIVATE-KEY)"
+      OIDC_MOCK_JWT_PUBLIC_KEY  = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-JWT-PUBLIC-KEY)"
+      OIDC_MOCK_FORCED_ENABLE   = true
+
+      NEXT_PUBLIC_IS_MSW_ENABLED  = true
+      NEXT_PUBLIC_BFF_API_MOCKING = true
+
+      NEXT_PUBLIC_BFF_API_BASE_URL  = "https://payments.ipatente.io.pagopa.it"
+      NEXT_PUBLIC_BFF_API_BASE_PATH = "/api"
+
+      EXT_API_MOCKING   = true
+      EXT_API_BASE_URL  = "http://example.com"
+      EXT_API_BASE_PATH = "/api"
+
+      INTEROP_AUTH_SERVER_ENDPOINT_URL = "https://auth.uat.interop.pagopa.it/token.oauth2"
+      INTEROP_CLIENT_ID                = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ID-PAYMENTS)"
+      INTEROP_CLIENT_ASSERTION_TYPE    = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+      INTEROP_GRANT_TYPE               = "client_credentials"
+
+      INTEROP_CLIENT_ASSERTION_KID        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-KID-PAYMENTS)"
+      INTEROP_CLIENT_ASSERTION_ISS        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-ISS-PAYMENTS)"
+      INTEROP_CLIENT_ASSERTION_SUB        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-SUB-PAYMENTS)"
+      INTEROP_CLIENT_ASSERTION_AUD        = "auth.uat.interop.pagopa.it / client-assertion"
+      INTEROP_CLIENT_ASSERTION_PURPOSE_ID = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-PURPOSE-ID-PAYMENTS)"
+      INTEROP_CLIENT_ASSERTION_PK         = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-PK-PAYMENTS)"
+
+      // Fetch keepalive
+      FETCH_KEEPALIVE_ENABLED             = "true"
+      FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL   = "110000"
+      FETCH_KEEPALIVE_MAX_SOCKETS         = "40"
+      FETCH_KEEPALIVE_MAX_FREE_SOCKETS    = "10"
+      FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
+      FETCH_KEEPALIVE_TIMEOUT             = "60000"
+
+      # Logs
+      AI_SDK_CONNECTION_STRING = var.ai_connection_string
+    }
+
+    prod_app_setting = {
+      APP_ENV = "production"
+    }
+
+    staging_app_setting = {
+      APP_ENV = "staging"
+    }
+
+    sticky_settings = [
+      "APP_ENV"
+    ]
+
+    autoscale_settings = {
+      min     = 3
+      max     = 30
+      default = 3
+    }
+  }
+}
---
+++ infra/resources/_modules/practices_app_service/README.md
@@ -0,0 +1,42 @@
+# practices_app_service
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+No requirements.
+
+## Providers
+
+No providers.
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_practices"></a> [practices](#module\_practices) | github.com/pagopa/dx//infra/modules/azure_app_service | main |
+
+## Resources
+
+No resources.
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_ai_connection_string"></a> [ai\_connection\_string](#input\_ai\_connection\_string) | Application Insights connection string | `string` | n/a | yes |
+| <a name="input_domain"></a> [domain](#input\_domain) | Domain name of the application | `string` | n/a | yes |
+| <a name="input_env_short"></a> [env\_short](#input\_env\_short) | n/a | `string` | n/a | yes |
+| <a name="input_key_vault_name"></a> [key\_vault\_name](#input\_key\_vault\_name) | Key Vault name | `string` | n/a | yes |
+| <a name="input_location"></a> [location](#input\_location) | Azure region | `string` | n/a | yes |
+| <a name="input_peps_snet_id"></a> [peps\_snet\_id](#input\_peps\_snet\_id) | Id of the subnet which holds private endpoints | `string` | n/a | yes |
+| <a name="input_practices_snet_cidr"></a> [practices\_snet\_cidr](#input\_practices\_snet\_cidr) | Licences Subnet CIDR | `string` | n/a | yes |
+| <a name="input_prefix"></a> [prefix](#input\_prefix) | n/a | `string` | `"io"` | no |
+| <a name="input_private_dns_zone_resource_group_name"></a> [private\_dns\_zone\_resource\_group\_name](#input\_private\_dns\_zone\_resource\_group\_name) | Resource group name of the private DNS zone to use for private endpoints | `string` | n/a | yes |
+| <a name="input_resource_group_name"></a> [resource\_group\_name](#input\_resource\_group\_name) | Resource group name for the Function App services | `string` | n/a | yes |
+| <a name="input_tags"></a> [tags](#input\_tags) | Resource tags | `map(any)` | n/a | yes |
+| <a name="input_virtual_network"></a> [virtual\_network](#input\_virtual\_network) | Virtual network to create subnet in | <pre>object({<br/>    name                = string<br/>    resource_group_name = string<br/>  })</pre> | n/a | yes |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
