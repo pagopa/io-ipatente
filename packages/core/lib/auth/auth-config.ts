dd6d935d675642ad26b305271f01a1164a69add3
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2024-12-10 12:49:51 +0100

    add redirect_uri

--- packages/core/lib/auth/auth-config.ts
+++ packages/core/lib/auth/auth-config.ts
@@ -65,6 +65,7 @@ export const authConfig: NextAuthConfig = {
     {
       authorization: {
         params: {
+          redirect_uri: getConfiguration().OIDC_CLIENT_REDIRECT_URI,
           scope: "openid profile lollipop",
         },
       },
