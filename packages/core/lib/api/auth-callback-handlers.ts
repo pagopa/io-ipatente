3120dd81cdb55f29ccd042762c0df4b07f61d1d6
Gianmaria Scorza <gianmaria@scorza.dev>
2024-12-11 10:40:48 +0100

    set cookie domain in auth callback

--- packages/core/lib/api/auth-callback-handlers.ts
+++ packages/core/lib/api/auth-callback-handlers.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from "next/server";
 
 import { NextResponse } from "next/server";
 
+import { getConfiguration } from "../config";
 import { FIMS_CALLBACK_URL } from "../utils";
 
 const PREFIX_COOKIE = "authjs.";
@@ -15,41 +16,49 @@ const handleAuthCallback = async (req: NextRequest): Promise<NextResponse> => {
     if (key.includes(PREFIX_COOKIE)) {
       console.log("FOUND: ", key);
       cookiesToBeDeleted.push(key);
-      const cookieKey = key.split(PREFIX_COOKIE)[1];
-      cookies[PREFIX_COOKIE.concat(cookieKey)] = value;
-
+      cookies[key] = value;
     }
   });
 
   console.log("COOKIES SELECTED: ", cookies);
-
   console.log("PRE-REDIRECT: ", req);
   console.log("PRE-REDIRECT-NEXT-URL: ", req.nextUrl);
   console.log("PRE-REDIRECT-NEXT-URL-ORIGIN: ", req.nextUrl.origin);
   console.log("PRE-REDIRECT-AUTH-URL: ", process.env.AUTH_URL);
   console.log(
     "PRE-REDIRECT-NEXT_PUBLIC_BFF_API_BASE_URL: ",
-    process.env.NEXT_PUBLIC_BFF_API_BASE_URL,
+    getConfiguration().BFF_API_BASE_URL,
   );
 
   req.nextUrl.pathname = FIMS_CALLBACK_URL;
-
 
   for (const cookie of cookiesToBeDeleted) {
     console.log("DELETING: ", cookie);
     req.nextUrl.searchParams.delete(cookie);
   }
 
+  const applicationDomain = getConfiguration().BFF_API_BASE_URL.replace(
+    "https://",
+    "",
+  );
+
   const redirectCallbackUrl = req.nextUrl.clone();
-  redirectCallbackUrl.host = "vehicles.ipatente.io.pagopa.it";
+  redirectCallbackUrl.host = applicationDomain;
   redirectCallbackUrl.port = "";
+
+  console.log("POST-CALLBACKURL-CALC: ", redirectCallbackUrl);
+  console.log("POST-CALLBACKURL-CALC-ORIGIN: ", redirectCallbackUrl.origin);
+
   const response = NextResponse.redirect(redirectCallbackUrl);
 
   Object.entries(cookies).forEach(([key, value]) => {
-    response.cookies.set(key, value);
+    response.cookies.set(key, value, {
+      domain: applicationDomain,
+    });
   });
 
   console.log("POST-REDIRECT: ", response);
+  console.log("POST-REDIRECT-COOKIES: ", response.cookies);
 
   return response;
 };
