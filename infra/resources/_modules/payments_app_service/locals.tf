65bfaeeb5cb8fd8e10d2840bcc6bc87cbcb4dc81
Gianmaria Scorza <gianmaria@scorza.dev>
2024-12-13 15:58:51 +0100

    [IOPAE-1604]Production configuration practices payments (#210)

--- infra/resources/_modules/payments_app_service/locals.tf
+++ infra/resources/_modules/payments_app_service/locals.tf
@@ -8,36 +8,37 @@ locals {
 
       AUTH_SECRET     = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=AUTH-SECRET-PAYMENTS)"
       AUTH_TRUST_HOST = true
-      AUTH_URL        = "https://io-p-itn-ipatente-payments-app-01.azurewebsites.net/"
+      AUTH_URL        = "https://payments.ipatente.io.pagopa.it/"
 
-      OIDC_ISSUER_URL          = "https://io-p-itn-ipatente-payments-app-01.azurewebsites.net/api/auth/mocks/oidc"
+      OIDC_ISSUER_URL          = "https://oauth.io.pagopa.it"
       OIDC_CLIENT_ID           = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-CLIENT-ID-PAYMENTS)"
       OIDC_CLIENT_SECRET       = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-CLIENT-SECRET-PAYMENTS)"
-      OIDC_CLIENT_REDIRECT_URI = "https://io-p-itn-ipatente-payments-app-01.azurewebsites.net/api/auth/callback/fims"
+      OIDC_CLIENT_REDIRECT_URI = "https://payments.ipatente.io.pagopa.it/api/auth/callback/fims"
 
       OIDC_MOCK_JWT_PRIVATE_KEY = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-JWT-PRIVATE-KEY)"
       OIDC_MOCK_JWT_PUBLIC_KEY  = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-JWT-PUBLIC-KEY)"
-      OIDC_MOCK_FORCED_ENABLE   = true
+      OIDC_MOCK_FORCED_ENABLE   = false
 
-      NEXT_PUBLIC_IS_MSW_ENABLED  = true
-      NEXT_PUBLIC_BFF_API_MOCKING = true
+      NEXT_PUBLIC_IS_MSW_ENABLED  = false
+      NEXT_PUBLIC_BFF_API_MOCKING = false
 
-      NEXT_PUBLIC_BFF_API_BASE_URL  = "https://io-p-itn-ipatente-payments-app-01.azurewebsites.net"
+      NEXT_PUBLIC_BFF_API_BASE_URL  = "https://payments.ipatente.io.pagopa.it"
       NEXT_PUBLIC_BFF_API_BASE_PATH = "/api"
 
-      EXT_API_MOCKING   = true
-      EXT_API_BASE_URL  = "http://example.com"
-      EXT_API_BASE_PATH = "/api"
+      EXT_API_MOCKING   = false
+      EXT_API_BASE_URL  = "https://gw.servizidt.it/rest/in/MCTC/IMieiPagamenti_AppIO"
+      EXT_API_BASE_PATH = "/v2"
 
-      INTEROP_AUTH_SERVER_ENDPOINT_URL = "https://auth.uat.interop.pagopa.it/token.oauth2"
+      INTEROP_AUTH_SERVER_ENDPOINT_URL = "https://auth.interop.pagopa.it/token.oauth2"
       INTEROP_CLIENT_ID                = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ID-PAYMENTS)"
       INTEROP_CLIENT_ASSERTION_TYPE    = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+      INTEROP_ESERVICE_AUDIENCE        = "https://gw.servizidt.it/rest/in/MCTC/IMieiPagamenti_AppIO/v2"
       INTEROP_GRANT_TYPE               = "client_credentials"
 
       INTEROP_CLIENT_ASSERTION_KID        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-KID-PAYMENTS)"
       INTEROP_CLIENT_ASSERTION_ISS        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-ISS-PAYMENTS)"
       INTEROP_CLIENT_ASSERTION_SUB        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-SUB-PAYMENTS)"
-      INTEROP_CLIENT_ASSERTION_AUD        = "auth.uat.interop.pagopa.it / client-assertion"
+      INTEROP_CLIENT_ASSERTION_AUD        = "auth.interop.pagopa.it/client-assertion"
       INTEROP_CLIENT_ASSERTION_PURPOSE_ID = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-PURPOSE-ID-PAYMENTS)"
       INTEROP_CLIENT_ASSERTION_PK         = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-PK-PAYMENTS)"
 
@@ -51,6 +52,10 @@ locals {
 
       # Logs
       AI_SDK_CONNECTION_STRING = var.ai_connection_string
+
+      # Internal API for test
+      INTERNAL_ROUTES_ENABLED   = false
+      INTERNAL_ROUTES_TEST_USER = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTERNAL-ROUTES-TEST-USER-PAYMENTS)"
     }
 
     prod_app_setting = {
--- infra/resources/_modules/practices_app_service/locals.tf
+++ infra/resources/_modules/practices_app_service/locals.tf
@@ -8,36 +8,37 @@ locals {
 
       AUTH_SECRET     = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=AUTH-SECRET-PRACTICES)"
       AUTH_TRUST_HOST = true
-      AUTH_URL        = "https://io-p-itn-ipatente-practices-app-01.azurewebsites.net/"
+      AUTH_URL        = "https://practices.ipatente.io.pagopa.it/"
 
-      OIDC_ISSUER_URL          = "https://io-p-itn-ipatente-practices-app-01.azurewebsites.net/api/auth/mocks/oidc"
+      OIDC_ISSUER_URL          = "https://oauth.io.pagopa.it"
       OIDC_CLIENT_ID           = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-CLIENT-ID-PRACTICES)"
       OIDC_CLIENT_SECRET       = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-CLIENT-SECRET-PRACTICES)"
-      OIDC_CLIENT_REDIRECT_URI = "https://io-p-itn-ipatente-practices-app-01.azurewebsites.net/api/auth/callback/fims"
+      OIDC_CLIENT_REDIRECT_URI = "https://practices.ipatente.io.pagopa.it/api/auth/callback/fims"
 
       OIDC_MOCK_JWT_PRIVATE_KEY = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-JWT-PRIVATE-KEY)"
       OIDC_MOCK_JWT_PUBLIC_KEY  = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=OIDC-JWT-PUBLIC-KEY)"
-      OIDC_MOCK_FORCED_ENABLE   = true
+      OIDC_MOCK_FORCED_ENABLE   = false
 
-      NEXT_PUBLIC_IS_MSW_ENABLED  = true
-      NEXT_PUBLIC_BFF_API_MOCKING = true
+      NEXT_PUBLIC_IS_MSW_ENABLED  = false
+      NEXT_PUBLIC_BFF_API_MOCKING = false
 
-      NEXT_PUBLIC_BFF_API_BASE_URL  = "https://io-p-itn-ipatente-practices-app-01.azurewebsites.net"
+      NEXT_PUBLIC_BFF_API_BASE_URL  = "https://practices.ipatente.io.pagopa.it"
       NEXT_PUBLIC_BFF_API_BASE_PATH = "/api"
 
-      EXT_API_MOCKING   = true
-      EXT_API_BASE_URL  = "http://example.com"
-      EXT_API_BASE_PATH = "/api"
+      EXT_API_MOCKING   = false
+      EXT_API_BASE_URL  = "https://gw.servizidt.it/rest/in/MCTC/LeMiePratiche_AppIO"
+      EXT_API_BASE_PATH = "/v1"
 
-      INTEROP_AUTH_SERVER_ENDPOINT_URL = "https://auth.uat.interop.pagopa.it/token.oauth2"
+      INTEROP_AUTH_SERVER_ENDPOINT_URL = "https://auth.interop.pagopa.it/token.oauth2"
       INTEROP_CLIENT_ID                = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ID-PRACTICES)"
       INTEROP_CLIENT_ASSERTION_TYPE    = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+      INTEROP_ESERVICE_AUDIENCE        = "https://gw.servizidt.it/rest/in/MCTC/LeMiePratiche_AppIO/v1"
       INTEROP_GRANT_TYPE               = "client_credentials"
 
       INTEROP_CLIENT_ASSERTION_KID        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-KID-PRACTICES)"
       INTEROP_CLIENT_ASSERTION_ISS        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-ISS-PRACTICES)"
       INTEROP_CLIENT_ASSERTION_SUB        = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-SUB-PRACTICES)"
-      INTEROP_CLIENT_ASSERTION_AUD        = "auth.uat.interop.pagopa.it / client-assertion"
+      INTEROP_CLIENT_ASSERTION_AUD        = "auth.interop.pagopa.it/client-assertion"
       INTEROP_CLIENT_ASSERTION_PURPOSE_ID = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-PURPOSE-ID-PRACTICES)"
       INTEROP_CLIENT_ASSERTION_PK         = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTEROP-CLIENT-ASSERTION-PK-PRACTICES)"
 
@@ -51,6 +52,10 @@ locals {
 
       # Logs
       AI_SDK_CONNECTION_STRING = var.ai_connection_string
+
+      # Internal API for test
+      INTERNAL_ROUTES_ENABLED   = false
+      INTERNAL_ROUTES_TEST_USER = "@Microsoft.KeyVault(VaultName=${var.key_vault_name};SecretName=INTERNAL-ROUTES-TEST-USER-PRACTICES)"
     }
 
     prod_app_setting = {
