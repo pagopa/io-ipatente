6686b777458556f17757a332b6daddd5aef250d0
Krusty93 <andrea.grillo@pagopa.it>
2025-02-06 09:48:26 +0100

    upgrade to azurerm v4

--- infra/resources/prod/providers.tf
+++ infra/resources/prod/providers.tf
@@ -10,11 +10,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.116.0"
+      version = "~>4"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.48.0"
+      version = "~>3"
     }
   }
 }
