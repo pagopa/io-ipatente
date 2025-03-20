95cbdb5b060efa45528e38ca7d9a13f966f31849
Christian Calabrese <christian.calabrese@pagopa.it>
2025-03-20 10:46:42 +0100

    Migrate to new terraform registry namespace (#265)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    
    <!--- Describe your changes in detail -->
    - The terraform registry namespace has changed from pagopa to pagopa-dx.
    - Terraform modules version constraints changed from `~> X` to `~> X.0`
    
    #### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    The terraform registry namespace has changed from pagopa to pagopa-dx.
    Moreover a fix over the modules version has been introduced to avoid
    unexpected usage of the next major version.
    
    #### How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

--- infra/resources/prod/data.tf
+++ infra/resources/prod/data.tf
@@ -6,11 +6,6 @@ data "azurerm_resource_group" "weu-common" {
   name = "${local.prefix}-${local.env_short}-rg-common"
 }
 
-data "azurerm_api_management" "apim_v2" {
-  name                = "${local.prefix}-${local.env_short}-apim-v2-api"
-  resource_group_name = "${local.prefix}-${local.env_short}-rg-internal"
-}
-
 # APIM itn
 
 data "azurerm_api_management" "apim_itn" {
