838ac09c2012e1d38db07bca4d7afede4bc304fd
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2025-03-25 12:14:01 +0100

    [IOPAE-1514] Add favicon (#272)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    
    #### List of Changes
    
    <!--- Describe your changes in detail -->
    
    #### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

--- apps/vehicles/src/pages/_document.tsx
+++ apps/vehicles/src/pages/_document.tsx
@@ -2,7 +2,11 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => (
   <Html lang="it">
-    <Head />
+    <Head>
+      <link href="/favicon.ico" rel="icon" sizes="32x32" />
+      <link href="/icon.svg" rel="icon" type="image/svg+xml" />
+      <link href="/apple-icon.png" rel="apple-touch-icon" />
+    </Head>
     <body>
       <Main />
       <NextScript />
