7c4cc6ef0ce0f5ea832f53bd89ab8c97f0dc3dc0
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2025-03-25 10:51:45 +0100

    Add favicon

--- apps/vehicles/src/pages/_document.tsx
+++ apps/vehicles/src/pages/_document.tsx
@@ -2,7 +2,11 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => (
   <Html lang="it">
-    <Head />
+    <Head>
+      <link rel="icon" href="/favicon.ico" sizes="32x32" />
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-icon.png" />
+    </Head>
     <body>
       <Main />
       <NextScript />
