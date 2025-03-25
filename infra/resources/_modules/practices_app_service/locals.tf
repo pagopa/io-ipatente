e3b5281d03d8f6a889d9a9acc76b4885d2ed5098
Stefano.D'aversa <stefano.daversa@pagopa.it>
2025-03-25 14:44:01 +0100

    update FETCH_KEEPALIVE_MAX_SOCKETS

--- infra/resources/_modules/practices_app_service/locals.tf
+++ infra/resources/_modules/practices_app_service/locals.tf
@@ -47,7 +47,7 @@ locals {
       // Fetch keepalive
       FETCH_KEEPALIVE_ENABLED             = "true"
       FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL   = "110000"
-      FETCH_KEEPALIVE_MAX_SOCKETS         = "40"
+      FETCH_KEEPALIVE_MAX_SOCKETS         = "100"
       FETCH_KEEPALIVE_MAX_FREE_SOCKETS    = "10"
       FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
       FETCH_KEEPALIVE_TIMEOUT             = "60000"
