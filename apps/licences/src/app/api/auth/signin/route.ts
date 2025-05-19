008a178f2dbf91cc6af70d10e67aaf767fe7e75a
Giuseppe Di Pinto <118166285+giuseppedipinto@users.noreply.github.com>
2025-05-19 12:32:57 +0200

    [IOPAE-2000] Updated consent page redirect (#309)
    <!--- Please always add a PR description as if nobody knows anything
    about the context these changes come from. -->
    <!--- Even if we are all from our internal team, we may not be on the
    same page. -->
    <!--- Write this PR as you were contributing to a public OSS project,
    where nobody knows you and you have to earn their trust. -->
    <!--- This will improve our projects in the long run! Thanks. -->
    Updated consent page redirect: wait until router isReady to set
    redirectPath.
    Cleanup logs.
    
    #### List of Changes
    
    <!--- Describe your changes in detail -->
    
    #### Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    #### How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, tests ran to see how
    -->
    <!--- your change affects other areas of the code, etc. -->
    
    #### Screenshots (if appropriate):
    
    <!--- Attach screenshots in case changes impact UI. -->
    
    #### Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.

--- apps/licences/src/app/api/auth/signin/route.ts
+++ apps/licences/src/app/api/auth/signin/route.ts
@@ -1,15 +1,7 @@
 import { signIn } from "@/auth";
 import { NextRequest } from "next/server";
 
-export const GET = async (request: NextRequest) => {
-  console.log("[signin] request: ", request);
-  console.log("[signin] request.nextUrl.pathname: " + request.nextUrl.pathname);
-
-  const url = new URL(request.url);
-  const callbackUrl = url.searchParams.get("callbackUrl") ?? url.href;
-  console.log("[signin] callbackUrl: " + callbackUrl);
-
-  return signIn("fims", {
+export const GET = async (request: NextRequest) =>
+  signIn("fims", {
     redirectTo: request.url,
   });
-};
