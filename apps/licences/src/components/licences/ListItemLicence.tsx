282f3b2ed5e008494d1385eed3e69b29bb19b2f6
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2025-06-19 12:34:47 +0200

    remove licence status badge

--- apps/licences/src/components/licences/ListItemLicence.tsx
+++ apps/licences/src/components/licences/ListItemLicence.tsx
@@ -1,6 +1,5 @@
 import { DatiPatente } from "@/generated/bff-openapi";
 import { LICENCE_TOTAL_POINTS } from "@/utils/constants";
-import { isDueDateValid } from "@io-ipatente/core";
 import { BadgeProps, ListItemAction } from "@io-ipatente/ui";
 import { useTranslation } from "next-i18next";
 import { useMemo } from "react";
@@ -18,18 +17,9 @@ export const ListItemLicence = ({
 }: ListItemLicenceProps) => {
   const { t } = useTranslation();
 
-  const { dataScadenza, numeroPatente, saldoPunti } = data;
+  const { numeroPatente, saldoPunti } = data;
 
   const badges = useMemo<BadgeProps[]>(() => {
-    const isValid = dataScadenza ? isDueDateValid(dataScadenza) : false;
-
-    const badgeLicenceStatus: BadgeProps = {
-      color: isValid ? "success" : "error",
-      icon: isValid ? "tickCircleBold" : "warning2Bold",
-      label: isValid ? t("licences.valid") : t("licences.expired"),
-      size: "small",
-    };
-
     const badgeLicencePoints: BadgeProps | undefined = saldoPunti
       ? {
           color: "info",
@@ -42,11 +32,8 @@ export const ListItemLicence = ({
         }
       : undefined;
 
-    return [
-      badgeLicenceStatus,
-      ...(badgeLicencePoints ? [badgeLicencePoints] : []),
-    ];
-  }, [dataScadenza, saldoPunti, t]);
+    return badgeLicencePoints ? [badgeLicencePoints] : [];
+  }, [saldoPunti, t]);
 
   return (
     <ListItemAction
