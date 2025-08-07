c8de3024ba26cd188eca509e4f8f3829860ab0e2
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2025-08-07 11:55:03 +0200

    update tracing packages

--- apps/practices/src/instrumentation.ts
+++ apps/practices/src/instrumentation.ts
@@ -1,13 +1,5 @@
-/**
- * https://nextjs.org/docs/app/building-your-application/optimizing/open-telemetry
- * https://pagopa.atlassian.net/wiki/spaces/DevEx/pages/1086619751/Azure+e+tracing+per+applicativi+NodeJS
- */
+import { initAzureMonitor } from "@pagopa/azure-tracing/azure-monitor";
+
 export async function register() {
-  if (
-    process.env.NODE_ENV === "production" &&
-    process.env.NEXT_RUNTIME === "nodejs"
-  ) {
-    await import("./instrumentation.node");
-    console.info("Instrumentation for Node.js runtime is set!");
-  }
+  initAzureMonitor();
 }
