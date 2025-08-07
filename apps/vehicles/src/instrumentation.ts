016740461fa57f1be360911c3b80a977f8aa2d58
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2025-08-07 13:14:20 +0200

    dynamic tracing module import only for serverside code

--- apps/vehicles/src/instrumentation.ts
+++ apps/vehicles/src/instrumentation.ts
@@ -1,5 +1,19 @@
-import { initAzureMonitor } from "@pagopa/azure-tracing/azure-monitor";
-
 export async function register() {
-  initAzureMonitor();
+  if (
+    process.env.NODE_ENV === "production" &&
+    process.env.NEXT_RUNTIME === "nodejs"
+  ) {
+    // Dynamically import the tracing module only if the code is running server-side on Node.js
+    const { initAzureMonitor } = await import(
+      "@pagopa/azure-tracing/azure-monitor"
+    );
+
+    initAzureMonitor();
+
+    console.log(
+      "Azure Monitor OpenTelemetry has been initialized on the server.",
+    );
+  } else {
+    console.log("Tracking code skipped on client.");
+  }
 }
