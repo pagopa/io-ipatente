90b44140825ed7430c1e177f3731bf684e6e1322
Krusty93 <andrea.grillo@pagopa.it>
2025-09-24 14:42:54 +0200

    new repository config

---
+++ infra/repository/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  repository = {
+    name               = "io-ipatente"
+    description        = "iPatente services"
+    topics             = ["io", "ipatente"]
+    reviewers_teams    = ["io-platform-green-unit", "engineering-team-cloud-eng"]
+    app_cd_policy_tags = ["io-ipatente-licences@*", "io-ipatente-payments@*", "io-ipatente-practices@*", "io-ipatente-vehicles@*"]
+  }
+}
