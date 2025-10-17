3d003d3b47ac16b4834c7c586d489172c5a42d71
giuseppedipinto <giuseppeantonio.dipinto@pagopa.it>
2025-10-17 10:28:53 +0200

    Update core unit tests with logger dependency injection

--- packages/core/lib/interop/__tests__/voucher.test.ts
+++ packages/core/lib/interop/__tests__/voucher.test.ts
@@ -1,11 +1,19 @@
 import axios from "axios";
 import { describe, expect, it, vi } from "vitest";
 
+import { CoreLogger } from "../../types/logger";
 import { Voucher, VoucherRequest, requestVoucher } from "../voucher";
 
 vi.mock("axios");
 
 describe("Voucher", () => {
+  const mockLogger: CoreLogger = {
+    debug: vi.fn(),
+    error: vi.fn(),
+    info: vi.fn(),
+    warn: vi.fn(),
+  };
+
   const mockVoucher: Voucher = {
     access_token: "test_access_token",
     expires_in: 3600,
@@ -26,7 +34,7 @@ describe("Voucher", () => {
   it("should send a POST request with the correct data and return a voucher", async () => {
     vi.mocked(axios.post).mockResolvedValueOnce({ data: mockVoucher });
 
-    const result = await requestVoucher(mockVoucherRequest);
+    const result = await requestVoucher(mockLogger)(mockVoucherRequest);
 
     expect(axios.post).toHaveBeenCalledWith(
       mockVoucherRequest.authServerEndpointUrl,
@@ -42,19 +50,23 @@ describe("Voucher", () => {
   });
 
   it("should handle errors and log them", async () => {
-    const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => {});
+    const loggerSpy = vi
+      .spyOn(mockLogger, "error")
+      .mockImplementation(() => {});
 
     vi.mocked(axios.post).mockRejectedValueOnce(new Error("Network error"));
 
-    const result = await requestVoucher(mockVoucherRequest);
+    const result = await requestVoucher(mockLogger)(mockVoucherRequest);
 
     expect(result).toBeUndefined();
-    expect(consoleSpy).toHaveBeenCalledWith(
+    expect(loggerSpy).toHaveBeenCalledWith(
       "An Error has occurred while requesting voucher, caused by: ",
-      expect.any(Error),
+      {
+        error: expect.any(Error),
+      },
     );
 
-    consoleSpy.mockRestore();
+    loggerSpy.mockRestore();
   });
 
   it("should send the correct URLSearchParams data", async () => {
@@ -68,7 +80,7 @@ describe("Voucher", () => {
 
     vi.mocked(axios.post).mockResolvedValueOnce({ data: mockVoucher });
 
-    await requestVoucher(mockVoucherRequest);
+    await requestVoucher(mockLogger)(mockVoucherRequest);
 
     expect(axios.post).toHaveBeenCalledWith(
       mockVoucherRequest.authServerEndpointUrl,
