1ed83785a54fada24819ae8200b5adeef267fd99
Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
2026-01-27 10:04:22 +0100

    fix reviewers teams (#355)

--- infra/repository/locals.tf
+++ infra/repository/locals.tf
@@ -3,7 +3,7 @@ locals {
     name               = "io-ipatente"
     description        = "iPatente services"
     topics             = ["io", "ipatente"]
-    reviewers_teams    = ["io-platform-green-unit", "engineering-team-cloud-eng"]
+    reviewers_teams    = ["io-institutions-n-services", "engineering-team-cloud-eng"]
     app_cd_policy_tags = ["io-ipatente-licences@*", "io-ipatente-payments@*", "io-ipatente-practices@*", "io-ipatente-vehicles@*"]
     jira_boards_ids    = ["IO-PAE", "CES"]
   }
