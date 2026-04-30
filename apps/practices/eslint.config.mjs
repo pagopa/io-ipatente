5bd5d65233531458a6e0693c3ca3dfafc4c1b3c6
giuseppedipinto <giuseppeantonio.dipinto@pagopa.it>
2026-04-30 16:51:23 +0200

    lint fix

--- apps/practices/eslint.config.mjs
+++ apps/practices/eslint.config.mjs
@@ -8,11 +8,12 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
+  allConfig: js.configs.all,
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 });
-export default [
+
+const config = [
   ...pagopa,
   ...compat.extends("next/core-web-vitals"),
   {
@@ -26,3 +27,5 @@ export default [
   },
   ...pluginQuery.configs["flat/recommended"],
 ];
+
+export default config;
