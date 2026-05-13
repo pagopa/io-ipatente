3efffd698b8495be0a218ac5d0b0f94f229164f0
Christian Calabrese <christian.calabrese@pagopa.it>
2026-05-13 10:10:59 +0200

    Update bootstrapper to streamline role assignments and enable GitHub app integration (#389)
    Refactor the bootstrapper configuration to reduce unnecessary role
    assignments and facilitate the use of a GitHub app for improved
    functionality. Update provider versions for better compatibility and
    performance.

--- infra/bootstrapper/prod/locals.tf
+++ infra/bootstrapper/prod/locals.tf
@@ -19,21 +19,6 @@ locals {
     }
   }
 
-  apim = {
-    name                = "${local.prefix}-${local.env_short}-itn-apim-01"
-    resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
-  }
-
-  apim_itn = {
-    name                = "${local.prefix}-${local.env_short}-itn-apim-01"
-    resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
-  }
-
-  vnet = {
-    name                = "${local.prefix}-${local.env_short}-itn-common-vnet-01"
-    resource_group_name = "${local.prefix}-${local.env_short}-itn-common-rg-01"
-  }
-
   dns = {
     resource_group_name = "${local.prefix}-${local.env_short}-rg-external"
   }
